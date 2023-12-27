bae1e5301d73e1e4899aa1114029f2ac67a76eba
Soumya-0x000 <soumyadas429@gmail.com>
2023-12-27 18:03:43 +0530

    home page completed

--- src/components/SocialMEdiaIcon/SocialMEdiaIcon.jsx
+++ src/components/SocialMEdiaIcon/SocialMEdiaIcon.jsx
@@ -4,20 +4,20 @@ import { faLinkedin, faGithub, faInstagram, faXTwitter, faMeta } from '@fortawes
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useTheme } from '../hook/ThemeContext'
 
-const iconLinks = [
-    {name: faLinkedin, color: '#0e8cec' },
-    {name: faInstagram, color: "#ed072a" },
-    {name: faGithub, color: '' },
-    {name: faXTwitter, color: '#d5a7ff' },
-    {name: faMeta, color: '#1A89C4' },
-]
-
 const SocialMEdiaIcon = () => {
     const {mode} = useTheme()
+    
+    const iconLinks = [
+        {name: faLinkedin, color: '#0e8cec' },
+        {name: faInstagram, color: "#ed072a" },
+        {name: faGithub, color: mode === 'dark' ? '#FFF17F' : '' },
+        {name: faXTwitter, color: mode === 'dark' ? '#d5a7ff' : '#CF95FF' },
+        {name: faMeta, color: mode === 'dark' ? '#59ABD6' : '#1A89C4' },
+    ]
 
     return (
         <span className='flex items-center justify-center py-5 2xl:py-0 fixed -bottom-2 right-1/2 2xl:right-8 translate-x-1/2 2xl:top-1/2 2xl:-translate-y-1/2 z-50'>
-            <span className={`flex items-center justify-between gap-x-9 ring-2 ${mode === 'dark' ? 'bg-[#1d7f3a88]' : 'bg-[#48ffc846]'} ring-green-400 2xl:flex-col backdrop-blur-[5px] z-20 2xl:px-2 2xl:py-7 2xl:gap-y-9 rounded-full w-fit px-6 py-1 `}>
+            <span className={`flex items-center justify-between gap-x-9 ring-2 ${mode === 'dark' ? '' : 'bg-[#48ffc846]'} ring-green-400 2xl:flex-col backdrop-blur-[5px] z-20 2xl:px-2 2xl:py-7 2xl:gap-y-9 rounded-full w-fit px-6 py-1 `}>
                 {iconLinks.map((item, index) => (
                     <motion.span whileHover={{y: -3}} className='w-6 flex items-center justify-center' key={index} >
                         <FontAwesomeIcon 
