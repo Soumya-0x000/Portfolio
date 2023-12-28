15766ab40e730134728e00f73eaab213ae7aea38
Soumya-0x000 <soumyadas429@gmail.com>
2023-12-28 20:22:21 +0530

    about section is under construction

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
     return (
         <div className={`${mode === 'dark' ? 'bg-darkSlate text-lighter' : ''} px-5 md:px-10 min-h-screen`}>
             {/* heading */}
-            <div className={`text-center text-[27px] sm:text-6xl md:text-[80px] font-extrabold 2xl:text-8xl pt-8 xl:pt-14 lg:pt-16 mx-10 lg:px-20 tracking-wide ${mode === 'dark' ? 'text-indigo-400' : 'text-indigo-500'}`}>
+            <div className={`text-center text-[27px] sm:text-6xl md:text-[80px] font-extrabold 2xl:text-8xl pt-8 xl:pt-14 lg:pt-16 lg:px-20 tracking-wide ${mode === 'dark' ? 'text-indigo-400' : 'text-indigo-500'}`}>
                 <TextReveal child={'Passion Fuels Purpose!'}/>
             </div>
             
