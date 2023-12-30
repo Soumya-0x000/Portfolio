b68d5a8fca4e3b206e67ff9e4334f6325a1a6f07
Soumya-0x000 <soumyadas429@gmail.com>
2023-12-30 21:38:58 +0530

    about section is under construction

--- src/components/about/pages/skills/Skills.jsx
+++ src/components/about/pages/skills/Skills.jsx
@@ -32,19 +32,20 @@ const shuffle = (array) => {
 };
 
 const squareData = [
-  {id: 1, src: html, shadow: 'shadow-orange-400', border: 'border-orange-400'},
-  {id: 2, src: css, shadow: 'shadow-blue-400', border:'border-blue-400'},
-  {id: 3, src: js, shadow: 'shadow-yellow-400', border:'border-yellow-400'},
-  {id: 4, src: redux, shadow: 'shadow-violet-400', border: 'border-violet-400'},
-  {id: 5, src: react, shadow: 'shadow-cyan-400', border: 'border-cyan-400'},
-  {id: 6, src: tailwind, shadow: 'shadow-sky-400', border: 'border-sky-400'},
-  {id: 7, src: sass, shadow: 'shadow-pink-400', border: 'border-pink-400'},
-  {id: 8, src: framer, shadow: 'shadow-violet-700', border: 'border-violet-700'},
-  {id: 9, src: Firebase, shadow: 'shadow-yellow-400', border: 'border-yellow-400'},
-  {id: 10, src: mui, shadow: 'shadow-blue-600', border: 'border-blue-600'},
-  {id: 11, src: git, shadow: 'shadow-red-400', border: 'border-red-400'},
-  {id: 12, src: github, shadow: 'shadow-slate-400', border: 'border-slate-400'},
+    {id: 1, name: 'HTML', src: html, shadow: 'shadow-orange-400', border: 'border-orange-400'},
+    {id: 2, name: 'CSS', src: css, shadow: 'shadow-blue-400', border:'border-blue-400'},
+    {id: 3, name: 'JavaScript', src: js, shadow: 'shadow-yellow-400', border:'border-yellow-400'},
+    {id: 4, name: 'Redux', src: redux, shadow: 'shadow-violet-400', border: 'border-violet-400'},
+    {id: 5, name: 'React', src: react, shadow: 'shadow-cyan-400', border: 'border-cyan-400'},
+    {id: 6, name: 'Tailwind CSS', src: tailwind, shadow: 'shadow-sky-400', border: 'border-sky-400'},
+    {id: 7, name: 'SASS', src: sass, shadow: 'shadow-pink-400', border: 'border-pink-400'},
+    {id: 8, name: 'Framer Motion', src: framer, shadow: 'shadow-violet-700', border: 'border-violet-700'},
+    {id: 9, name: 'Firebase', src: Firebase, shadow: 'shadow-yellow-400', border: 'border-yellow-400'},
+    {id: 10, name: 'Material-UI', src: mui, shadow: 'shadow-blue-600', border: 'border-blue-600'},
+    {id: 11, name: 'Git', src: git, shadow: 'shadow-red-400', border: 'border-red-400'},
+    {id: 12, name: 'GitHub', src: github, shadow: 'shadow-slate-400', border: 'border-slate-400'},
 ];
+  
 
 const generateSquares = (mode) => {
     return (
@@ -53,9 +54,9 @@ const generateSquares = (mode) => {
             key={sq.id}
             layout
             transition={{ duration: 1.5, type: "spring" }}
-            className={`w-[70px] sm:w-[80px] lg:w-[95px] h-[70px] sm:h-[80px] lg:h-[95px] rounded-full bg-center object-cover object-center p-[2px] cursor-pointer shadow-lg ${sq.shadow} `}
+            className={`w-[70px] sm:w-[80px] lg:w-[95px] 2xl:w-[110px] h-[70px] sm:h-[80px] lg:h-[95px] 2xl:h-[110px] rounded-full bg-center object-cover object-center p-[2px] cursor-pointer shadow-lg ${sq.shadow} mx-auto `}
             >
-                <div className={`bg-slate p-2 rounded-full h-full w-full flex items-center justify-center overflow-hidden`}>
+                <div className={`bg-slate p-2 rounded-full h-full w-full flex items-center justify-center overflow-hidden hover:bg-slate-800 transition-all`}>
                     <img src={sq.src} className='w-full h-full'/>
                 </div>
             </motion.div>
@@ -81,7 +82,7 @@ const ShuffleGrid = () => {
     };
 
     return (
-        <div className="w-full text-center grid grid-cols-3 sm:grid-cols-4 xl:grid-cols-6 ring items-center justify-center gap-y-6 gap-x-5 lg:gap-y-[3rem] ">
+        <div className="w-full grid grid-cols-3 sm:grid-cols-4 2xl:grid-cols-6 gap-y-6 lg:gap-y-[4rem] gap-x-5">
             {squares.map((sq) => sq)}
         </div>
     );
@@ -91,7 +92,7 @@ const Skills = () => {
     const {mode} = useTheme()
 
     return (
-        <div className='flex items-center flex-col gap-y-8 sm:gap-y-[50px] mt-20 '>
+        <div className='flex items-center flex-col gap-y-8 sm:gap-y-[50px] mt-20 pb-24'>
             <span className='flex items-center justify-center bo rounded-full'>
                 <motion.span 
                 className={`px-6 md:px-7 xl:px-9 py-1 md:py-2 xl:py-3 text-lg sm:text-xl lg:text-[25px] font-semibold rounded-full ${mode === 'dark' ? 'bg-slate-800' : 'bg-slate-300'}`}
