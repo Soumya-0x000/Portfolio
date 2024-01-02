2f9ab57c4f02069d1ef849685c836950337c3e89
Soumya-0x000 <soumyadas429@gmail.com>
2024-01-02 09:28:43 +0530

    education section is under construction

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
     const isInView = useInView(containerRef, {once: true})
     
     return (
-        <div className={`${mode === 'dark' ? 'bg-darkSlat text-lighter' : ''} px-5 md:px-10 min-h-screen pt-[130px] sm:pt-[60px]`}>
+        <div className={`${mode === 'dark' ? 'bg-darkSlat text-lighter' : ''} px-5 md:px-10 min-h-screen pt-[70px] sm:pt-[60px]`}>
             <StarsCanvas/>
             {/* heading */}
             <div className={` text-center text-[27px] sm:text-6xl md:text-[80px] font-extrabold 2xl:text-8xl pt-8 xl:pt-14 lg:pt-16 lg:px-20 tracking-wide ${mode === 'dark' ? 'text-indigo-400' : 'text-indigo-500'}`}
