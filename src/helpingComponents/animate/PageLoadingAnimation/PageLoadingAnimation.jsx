caf5afead0a55f6185a5dd81d092465c88a3d310
Soumya-0x000 <soumyadas429@gmail.com>
2024-02-04 10:49:25 +0530

    ux improved

--- src/helpingComponents/animate/PageLoadingAnimation/PageLoadingAnimation.jsx
+++ src/helpingComponents/animate/PageLoadingAnimation/PageLoadingAnimation.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './PageLoadingAnimation.css'
+import { useTheme } from '../../hook/ThemeContext'
 
 const PageLoadingAnimation = () => {
+    const {mode} = useTheme()
+
     return (
-        <div className='fixed flex items-center justify-center top-0 left-0 w-full h-screen z-30 bg-slate-800'>
+        <div className={`fixed flex items-center justify-center top-0 left-0 w-full h-screen z-30 ${mode === 'dark' ? 'bg-slate-800' : 'bg-slate-100'}`}>
             <div className="loader">
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
-                <div className="dot"></div>
+                <div className={`dot border-[2px] ${mode === 'dark' ? 'border-slate-800' : 'border-slate-100' } `}></div>
+                <div className={`dot border-[2px] ${mode === 'dark' ? 'border-slate-800' : 'border-slate-100' } `}></div>
+                <div className={`dot border-[2px] ${mode === 'dark' ? 'border-slate-800' : 'border-slate-100' } `}></div>
+                <div className={`dot border-[2px] ${mode === 'dark' ? 'border-slate-800' : 'border-slate-100' } `}></div>
+                <div className={`dot border-[2px] ${mode === 'dark' ? 'border-slate-800' : 'border-slate-100' } `}></div>
             </div>
         </div>
     )
